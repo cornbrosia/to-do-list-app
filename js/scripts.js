cb46eb5d7af3c28b1e4e2be13468137c21b31018
rbenwatson <rbenwatson@gmail.com>
2024-05-19 16:46:18 -0400

    current

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ function newItem() {
 
     //javascript
     //1. Adding a new item to the list of items: 
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("input").value;
-    let text = document.createTextNode(inputValue);
-    li.appendChild(text);
+    // let li = document.createElement("li");
+    // let inputValue = document.getElementById("input").value;
+    // let text = document.createTextNode(inputValue);
+    // li.appendChild(text);
 
+    let inputValue = $('#input').val();
+    $("ol").append("<li>" + inputValue + "</li>");
     if (inputValue === '') {
         alert("You must write something!");
     } else {
